9e24a8e11d7c5bd876eadab4b9045857d28d392f
Rezoo7 <maxime.guigourez@gmail.com>
2019-11-25 17:50:31 +0100

    test json request fail

--- app/src/main/java/com/example/mobileproj/MainActivity.java
+++ app/src/main/java/com/example/mobileproj/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.mobileproj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        final ConstraintLayout fond = (ConstraintLayout) findViewById(R.id.fond);
+
+        fond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
