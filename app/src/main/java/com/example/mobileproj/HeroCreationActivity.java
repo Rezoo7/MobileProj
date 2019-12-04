ce8deec17bbd711622f44883be295f74e2075c45
E19G654R <remi.thomas@etu.univ-nantes.fr>
2019-12-04 09:42:56 +0100

    feat: hero creation view with image chooser and licence switch

--- app/src/main/java/com/example/mobileproj/HeroCreationActivity.java
+++ app/src/main/java/com/example/mobileproj/HeroCreationActivity.java
@@ -2,14 +2,21 @@ package com.example.mobileproj;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,15 +25,20 @@ import androidx.core.content.ContextCompat;
 
 public class HeroCreationActivity extends AppCompatActivity {
 
+    final int PICK_IMAGE_FLAG = 222;
+
     //region LAYOUT ELEMENTS
     private Button createBtn;
     private ImageView globalIV, starIV, addEquipIV, addFilmIV;
     private EditText nameInput, homeworldInput, bdayInput, sizeInput, weightInput;
     private Spinner genderSpinner;
+    private Switch licenceSwitch;
     //endregion
 
     //region DATA
     private boolean isFav = true;
+    private boolean isSW = true;
+    private String mainImgPath;
     private List<Equipment> equipments;
     private List<Film> films;
     //endregion
@@ -48,10 +60,23 @@ public class HeroCreationActivity extends AppCompatActivity {
         sizeInput = findViewById(R.id.sizeInput);
         weightInput = findViewById(R.id.weightInput);
         genderSpinner = findViewById(R.id.genderSpinner);
+        licenceSwitch = findViewById(R.id.licenceSwitch);
         //endregion
 
         equipments = new ArrayList<>();
         films = new ArrayList<>();
+
+
+        globalIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_FLAG);
+            }
+        });
+
 
         starIV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +88,7 @@ public class HeroCreationActivity extends AppCompatActivity {
                 starIV.setImageDrawable(ContextCompat.getDrawable(view.getContext(), resID));
             }
         });
+
 
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,12 +124,39 @@ public class HeroCreationActivity extends AppCompatActivity {
                 //region Send infos
                 // TODO : fromSW & imgPath
                 Intent intent = new Intent();
-                Hero hero = new Hero(true, isFav, heroName, homeworld, gender, bday, size, weight, "", equipments, films);
+                Hero hero = new Hero(isSW, isFav, heroName, homeworld, gender, bday, size, weight, mainImgPath, equipments, films);
                 intent.putExtra("CreatedHero", hero);
                 setResult(Activity.RESULT_OK, intent);
                 finish();
                 //endregion
             }
         });
+
+        licenceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+                isSW = !isChecked;
+                licenceSwitch.setText((isSW ? "Star Wars" : "Marvel"));
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_FLAG && resultCode == Activity.RESULT_OK) {
+            try {
+                InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                globalIV.setImageBitmap(myBitmap);
+                mainImgPath = inputStream.toString();
+                // TODO create inputStream from string : InputStream is = new ByteArrayInputStream(StandardCharsets.UTF_16.encode(myString).array());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
